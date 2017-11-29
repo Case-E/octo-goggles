41fd4bd3fd01114e87b2ed40d96b7712b0d4dc36
KC <chaitanya@krishnachaitanya.me>
2017-11-29 09:18:44 +0530

    Can I be a developer?

--- FizzBuzz/fizzBuzz.js
+++ FizzBuzz/fizzBuzz.js
@@ -1,8 +1,12 @@
+//initializing the array with the number 1
 var nums = [1];
 
+//Adding numbers until 100
 while (nums.length < 101) {
     nums.push(nums[nums.length-1] + 1);
 }
+
+//Am I a developer? Can I be one? Do I pass the fizzBuzz test?
 nums.forEach(num => {
     if (num % 3 === 0 && num % 5 === 0) {
         console.log("fizzBuzz");
@@ -17,3 +21,5 @@ nums.forEach(num => {
         console.log(num);
     }
 });
+
+//The End.
